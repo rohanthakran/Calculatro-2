fef7b772a42ffa7af0ae4aeaa1bb6951fb24c4e1
Rohan <31851961+rohanthakran@users.noreply.github.com>
2020-03-25 13:27:19 +0530

    Update roh.js

--- roh.js
+++ roh.js
@@ -10,3 +10,8 @@ function equal(){
        document.form.textview.value = eval(exp);
     }
 }
+
+function clean(){
+    var exp = document.form.textview.value;
+    document.form.textview.value= exp.substring(0, exp.length-1);
+}
